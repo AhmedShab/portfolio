9484eb85d1f022e1f22fbb19335361cad21f64a6
AhmedShab <ahmed.vuw@gmail.com>
2016-09-28 17:19:29 +1300

    using react bootstrap insead

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import NavLink from './NavLink';
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <ul role="Nav">
+    const navbarInstance = (
+    <Navbar inverse>
+      <Navbar.Collapse>
+        <Nav>
           <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
-        </ul>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+    return (
+      <div>
+        {navbarInstance}
         {this.props.children}
       </div>
     );
