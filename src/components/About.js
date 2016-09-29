1b90bf6a845a65fe42ceb6841789988cb0c402c5
AhmedShab <ahmed.vuw@gmail.com>
2016-09-29 16:37:13 +1300

    fixed the arrow button in about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,19 +6,20 @@ import NavLink from './NavLink';
 export default class About extends Component {
   render() {
     return (
-      <div className="about">
-        <h2>
-          Hello,
-          <br/><br/>
-          My name is Ahmed. I'm a full stack web developer.
-          <br/><br/>
-          Making web applications is my passion. I enjoy my work and always looking forward to create new project
-        </h2>
-
-        <NavLink to="/work">
+      <div>
+        <div className="about">
+          <h2>
+            Hello,
+            <br/><br/>
+            My name is Ahmed. I'm a full stack web developer.
+            <br/><br/>
+            Making web applications is my passion. I enjoy my work and always looking forward to create new projects
+          </h2>
+        </div>
+        <NavLink className="next" to="/work">
           <FontAwesome
             name='a fa-arrow-circle-right'
-            size='4x'
+            size='5x'
             />
         </NavLink>
       </div>
