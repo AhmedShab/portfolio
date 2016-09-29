181bf0b6f696016ff5478b3e02743392c2bda584
AhmedShab <ahmed.vuw@gmail.com>
2016-09-29 14:27:18 +1300

    added images to my projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+
 export default class Work extends Component {
   render() {
+    const imageShapeInstance = (
+      <Grid>
+        <Row>
+          <Col xs={3} md={3}>
+            <Image src="images/echo.png" thumbnail />
+          </Col>
+          <Col xs={3} md={3}>
+            <Image src="images/foodBytes.png" thumbnail />
+          </Col>
+          <Col xs={3} md={3}>
+            <Image src="images/blog.png" thumbnail />
+          </Col>
+        </Row>
+      </Grid>
+    );
+
     return (
       <div className="work">
-        <h1>Work page</h1>
+        <div>
+          <h2>Check out my projects</h2>
+        </div>
+        {imageShapeInstance}
       </div>
     );
   }
