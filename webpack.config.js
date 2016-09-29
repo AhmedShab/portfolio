7b4981610a13c48475f6305360972f2079daeca0
AhmedShab <ahmed.vuw@gmail.com>
2016-09-30 00:09:59 +1300

    fixing my deployment error

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,9 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
